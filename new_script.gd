ecd6d5117bc63eb09f19e576087c3ed71c82fb05
Suorsa Sofia <jenna.suorsa@edu.espoo.fi>
2025-11-01 23:36:12 +0200

    16 squares

--- new_script.gd
+++ new_script.gd
@@ -25,3 +25,31 @@ func _ready():
 		btn.text = data["word"]
 		btn.connect("pressed", Callable(self, "_on_word_pressed").bind(btn))
 		grid.add_child(btn) 
+func on_word_pressed(btn):
+	if btn in selected_buttons:
+		btn.modulate = Color. DARK_SLATE_BLUE
+		selected_buttons.erase(btn)
+	else :
+		if selected_buttons.size() < 4:
+			btn.modulate = Color.ALICE_BLUE
+			selected_buttons.append(btn)
+		if selected_buttons.size() == 4:
+			_check_selection()
+func _check_selection():
+	var groups = []
+	for btn in selected_buttons:
+		var word_data = words.find({"word": btn.text, "group": null}) #ei toimi näin suoraan, parempi:
+		for data in words:
+			if data["word"] == btn.text:
+				groups.append(data["group"])
+				break
+	if groups.all(func(g): return g == groups[0]):
+		message_label.text= "Oikein! Ryhmä: %s" % groups[0]
+		for btn in selected_buttons:
+			btn.disabled = true
+			btn.modulate = Color.DARK_MAGENTA
+	else :
+		message_label.text = "Väärin! Yritä uudestaan."
+		for btn in selected_buttons :
+			btn.modulate = Color.CRIMSON
+			selected_buttons.clear()
